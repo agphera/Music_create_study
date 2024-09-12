5acea74ccc1f72db7a8386ea5d123d989f7ebe4a
agphera <agphera2@gmail.com>
2024-09-12 09:35:03 +0900

    third trial_6case

--- music_create_torch.py
+++ music_create_torch.py
@@ -20,17 +20,17 @@ text_prompts = [
     "Christian music with acoustic guitar and sacred atmosphere" #찬송가
 ]
 
-# 3. 음악 생성 및 저장
-for i, prompt in enumerate(text_prompts, start=1):  # i는 1부터 시작
-    # 입력 텍스트를 처리
+# 음악 생성 및 저장
+for i, prompt in enumerate(text_prompts, start=1): 
+    # 입력 텍스트 처리
     inputs = processor(
-        text=[prompt],  # 각 프롬프트를 하나씩 처리
+        text=[prompt],  
         padding=True,
         return_tensors="pt",
     )
     
     # 모델을 사용해 음악 생성
-    with torch.no_grad():
+    with torch.no_grad(): 
         audio_values = model.generate(**inputs, do_sample=True, guidance_scale=3, max_new_tokens=256)
     
     # 샘플링 레이트 가져오기
